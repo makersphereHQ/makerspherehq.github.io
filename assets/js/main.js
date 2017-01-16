4126f00ee1c352c491e8b064646ad4b484e49bce
Makersphere Labs <opensource@makersphere.org>
2017-01-17 00:27:03 +0100

    Add StatusPage & minor updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,12 @@ var headroom = new Headroom(header, {
     offset : 205
 });
 headroom.init();
+// StatusPage
+var sp = new StatusPage.page({ page : 'srkfthnyhcz9' });
+sp.status({
+    success : function(data) {
+        console.log(data.status.indicator);
+        document.getElementById('footer__status-desc').innerHTML = data.status.description;
+        document.getElementById('footer__status-dot').classList.add(data.status.indicator);
+    }
+});
